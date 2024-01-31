4e9998cc8cdac1bf5c4d1603410547fb874e1827
Ryan Jeynes <ryanjeynes@outlook.com>
2024-01-31 16:58:31 +0000

    Tidying up
    Moved content from sections 1 and 2 to the Home route

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import User from "./User";
+import Paragraph from "./Paragraph";
+
+const h1Style = {
+  background: "blue",
+  color: "#fff"
+};
+
+const RANKS = {
+  b: "Beginner",
+  e: "Expert"
+};
 
 function Home()
 {
     return <>
-        <h3>Home page</h3>
+        <h1 style={h1Style}>{`I am currently a ${RANKS.b} at React, working towards becoming an ${RANKS.e}`}</h1>
+        <User name="Tester" status="Unverified"/>
+        <Paragraph/>
     </>
 }
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Paragraph from './Paragraph';
-import User from './User';
 import { BrowserRouter } from 'react-router-dom';
-
-const h1Style = {
-  background: "blue",
-  color: "#fff"
-};
-
-const RANKS = {
-  b: "Beginner",
-  e: "Expert"
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <h1 style={h1Style}>{`I am currently a ${RANKS.b} at React, working towards becoming an ${RANKS.e}`}</h1>
-      <User name="Tester" status="Unverified"/>
-      <Paragraph/>
       <App/>
     </BrowserRouter>
   </React.StrictMode>
