4cc152b7b375a8f98b8c8b81b4842279414a6ed4
Ryan Jeynes <ryanjeynes@outlook.com>
2024-01-31 21:19:35 +0000

    Added the Calculator to the Routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Header from './Components/Header';
 import Counter from './Components/Counter';
+import Calculator from './Components/Calculator';
 import {Routes, Route} from 'react-router-dom';
 
 function App(){
@@ -18,6 +19,7 @@ function App(){
                 }/>
                 <Route path = "/counter" element={<Counter/>}/>
                 <Route path = "/about" element={<About/>}/>
+                <Route path = "/calculator" element={<Calculator/>}/>
         </Routes>
         </>
     );
